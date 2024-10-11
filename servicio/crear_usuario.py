e7df9471822bd2a7216bdcfb03cb4d94bdd5ac50
RollingCC121 <daniel.gaviria0605@gmail.com>
2024-10-11 13:39:26 -0500

    terminando los end point

--- servicio/crear_usuario.py
+++ servicio/crear_usuario.py
@@ -7,8 +7,18 @@ def crear_usuario(correo: str, password: str):
     cursor = connection.conn.cursor()
     
     try:
+        # Verificar si el usuario ya está registrado
+        cursor.execute("SELECT * FROM usuarios WHERE correo = %s", (correo,))
+        usuario_existente = cursor.fetchone()
+        
+        if usuario_existente:
+            raise HTTPException(status_code=400, detail="El usuario ya está registrado")
+        
+        # Insertar el nuevo usuario
         cursor.execute("INSERT INTO usuarios (correo, password) VALUES (%s, %s)", (correo, password))
         connection.conn.commit()
+    except HTTPException as http_exc:
+        raise http_exc
     except Exception as e:
         connection.conn.rollback()
         raise Exception(f"Error al crear el usuario: {str(e)}")
